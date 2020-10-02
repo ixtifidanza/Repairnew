9036bac72e2ee7cbc8016a9179c8afb46abb5f26
ixtifidanza <ixtifidanza@gmail.com>
2020-10-03 00:24:51 +0500

    add price

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,55 @@
-$(function(){
+$(function () {
+
+    // Slider
+
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+    
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    // Modal
 
     var button = $('#btn');
     var modal = $('#modal');
     var close = $('#close');
+    var dialog = $('.modal-dialog');
 
-    button.on('click', function() {
-        modal.addClass('modal_active');
+    modal.hide();    
+
+    button.on('click', function () {
+        modal.show();
+        dialog.toggleClass('modal-dialog__active');
     });
 
-    close.on('click', function() {
-        modal.removeClass('modal_active');
+    close.on('click', function () {
+        modal.hide();
+        dialog.toggleClass('modal-dialog__active');
     });
 
 });
